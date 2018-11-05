d572456f474a376ca5cd6d5383de3f19f333a1f8
Artem Berezynets <artem9825@gmail.com>
2018-11-05 23:47:45 +0200

    Added some regular tests

--- test/main_test.cpp
+++ test/main_test.cpp
@@ -15,6 +15,36 @@ using ::testing::_;
 using ::testing::AtLeast;
 
 
+TEST (CharDisplayTest, CheckIsLetter) {
+    ASSERT_TRUE(isLetter('m'));
+    ASSERT_FALSE(isLetter('&'));
+    ASSERT_TRUE(isLetter('R'));
+}
+
+
+TEST (CharDisplayTest, CheckIsNumber) {
+    ASSERT_FALSE(isNumber(0));
+    ASSERT_FALSE(isNumber('n'));
+    ASSERT_TRUE(isNumber('5'));
+}
+
+
+TEST (TagTest, EmptyTagTest) {
+    Tag emptyTag{Tag::createEmpty()};
+
+    ASSERT_EQ(0, emptyTag());
+}
+
+
+TEST (TagTest, TwoTagsTest) {
+    Tag tag1{Tag::createNew()};
+    Tag tag2{Tag::createNew()};
+
+    ASSERT_NE(0, tag2());
+    ASSERT_NE(tag1(), tag2());
+    ASSERT_EQ(tag1() + 1, tag2());
+}
+
 
 
 int main(int argc, char **argv)
