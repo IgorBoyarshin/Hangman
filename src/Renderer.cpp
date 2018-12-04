c45f8b2c582f5d8dee2097eb03ebd4898b36a09a
Igor Boyarshin <igor.boyarshin@gmail.com>
2018-12-04 20:12:31 +0200

    Added more special chars to display

--- src/Renderer.cpp
+++ src/Renderer.cpp
@@ -41,6 +41,84 @@ void Renderer::put(SpecialChar specialChar) {
         case SpecialChar::VLINE:
             addch(ACS_VLINE);
             break;
+        case SpecialChar::LTEE:
+            addch(ACS_LTEE);
+            break;
+        case SpecialChar::RTEE:
+            addch(ACS_RTEE);
+            break;
+        case SpecialChar::BTEE:
+            addch(ACS_BTEE);
+            break;
+        case SpecialChar::TTEE:
+            addch(ACS_TTEE);
+            break;
+        case SpecialChar::PLUS:
+            addch(ACS_PLUS);
+            break;
+        case SpecialChar::S1:
+            addch(ACS_S1);
+            break;
+        case SpecialChar::S3:
+            addch(ACS_S3);
+            break;
+        case SpecialChar::S7:
+            addch(ACS_S7);
+            break;
+        case SpecialChar::S9:
+            addch(ACS_S9);
+            break;
+        case SpecialChar::DIAMOND:
+            addch(ACS_DIAMOND);
+            break;
+        case SpecialChar::CKBOARD:
+            addch(ACS_CKBOARD);
+            break;
+        case SpecialChar::DEGREE:
+            addch(ACS_DEGREE);
+            break;
+        case SpecialChar::PLMINUS:
+            addch(ACS_PLMINUS);
+            break;
+        case SpecialChar::BULLET:
+            addch(ACS_BULLET);
+            break;
+        case SpecialChar::LARROW:
+            addch(ACS_LARROW);
+            break;
+        case SpecialChar::RARROW:
+            addch(ACS_RARROW);
+            break;
+        case SpecialChar::DARROW:
+            addch(ACS_DARROW);
+            break;
+        case SpecialChar::UARROW:
+            addch(ACS_UARROW);
+            break;
+        case SpecialChar::BOARD:
+            addch(ACS_BOARD);
+            break;
+        case SpecialChar::LANTERN:
+            addch(ACS_LANTERN);
+            break;
+        case SpecialChar::BLOCK:
+            addch(ACS_BLOCK);
+            break;
+        case SpecialChar::LEQUAL:
+            addch(ACS_LEQUAL);
+            break;
+        case SpecialChar::GEQUAL:
+            addch(ACS_GEQUAL);
+            break;
+        case SpecialChar::PI:
+            addch(ACS_PI);
+            break;
+        case SpecialChar::NEQUAL:
+            addch(ACS_NEQUAL);
+            break;
+        case SpecialChar::STERLING:
+            addch(ACS_STERLING);
+            break;
         default:
             break;
             // TODO: error
